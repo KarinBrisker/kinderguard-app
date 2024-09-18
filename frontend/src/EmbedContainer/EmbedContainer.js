ec159035b282c3fed55c934ab46b0f6872e73cd0
Nofar Oren Edan <noedan@microsoft.com>
2024-09-18 17:17:49 +0300

    add custom insights update

--- frontend/src/EmbedContainer/EmbedContainer.js
+++ frontend/src/EmbedContainer/EmbedContainer.js
@@ -8,6 +8,33 @@ export function EmbedContainer(data) {
         return `https://www.videoindexer.ai/embed/insights/${data.accountId}/${data.videoId}?location=${data.location}`;
       };
 
+      // Jump to specific time from message payload
+      const notifyWidgets = (evt) => {
+        if (!evt) {
+            return;
+        }
+        var origin = evt.origin || evt.originalEvent.origin;
+
+        // Validate that event comes from video indexer domain.
+        if ((origin.indexOf(".videoindexer.ai") !== -1 || origin.indexOf("videoindexer.ai.azure.us") !== -1) ) {
+
+            // Pass message to other iframe.
+            if ('postMessage' in window) {
+                var iframes = window.document.getElementsByTagName('iframe');
+                try {
+                    for (var index = 0; index < iframes.length; index++) {
+                        iframes[index].contentWindow.postMessage(evt.data, origin);
+                    }
+                } catch (error) {
+                    throw error;
+                }
+            }
+        }
+      }
+
+    // Listen to message events from breakdown iframes
+    window.addEventListener("message", notifyWidgets, false);
+
     return (
       <div>
          <iframe title="insights" width="580" height="780" src={getInsightsUrl()} frameborder="0" allowfullscreen></iframe>
--- frontend/src/upload-container/UploadContainer.js
+++ frontend/src/upload-container/UploadContainer.js
@@ -35,7 +35,7 @@ export function UploadContainer(props) {
 
     const handleFileUpload = async (event) => {
         const file = event.target.files[0];
-        if (file?.type === 'audio/wav') {
+        if (file?.type === 'audio/wav' || file?.type === 'audio/mp3' || file?.type === 'video/mp4') {
             // Handle the file upload logic here
             console.log('File uploaded:', file);
             setIsLoading(true);
@@ -44,7 +44,7 @@ export function UploadContainer(props) {
             // Uploaded
             setIsUploaded(true);
         } else {
-            console.log('Please upload a WAV file.');
+            console.log('Please upload a WAV/MP4/MP3 file.');
         }
     };
 
@@ -60,7 +60,7 @@ export function UploadContainer(props) {
         formData.append('file', file);
 
         try {
-            const response = await fetch(`https://api.videoindexer.ai/${location}/Accounts/${accountId}/Videos?name=audio_test&privacy=public&indexingPreset=AdvancedAudio`, {
+            const response = await fetch(`https://api.videoindexer.ai/${location}/Accounts/${accountId}/Videos?name=audio_test&privacy=public&indexingPreset=AdvancedAudio&language=he-IL`, {
                 method: 'POST',
                 headers: {
                     'Authorization': `Bearer ${token}`,
@@ -99,6 +99,7 @@ export function UploadContainer(props) {
             console.log('Video status:', data);
 
             if (data?.state === 'Processed') {
+                await findBadWords(data);
                 setVideoId(currentVID);
                 setIsLoading(false);
             } else {
@@ -110,6 +111,153 @@ export function UploadContainer(props) {
             console.error('Error fetching video status:', error);
         }
     };
+
+    const findBadWords = async (videoIndex) =>{
+        const badWords = ["טיפש",
+            "טיפשון",
+            "טיפשה",
+            "טיפשים",
+            "טיפשות",
+            "מכוער",
+            "מכוערת",
+            "מכוערים",
+            "מכוערות",
+            "שמן",
+            "שמנה",
+            "בכיין",
+            "מעצבן",
+            "קרציה",
+            "מכוער",
+            "אידיוט",
+            "זוז",
+            "זוזו",
+            "תעוף מפה",
+            "לא מקבל",
+            "לא מקבלת",
+            "עונש",
+            "די",
+            "כוס אמא שלך",
+            "כוס אמא",
+            "תמות",
+            "תמותי",
+            "תמותו",
+            "תיחנק",
+            "תיחנקי",
+            "תיחנקו",
+            "סתום",
+            "סתמי",
+            "סתמו",
+            "סתומה",
+            "סתומות",
+            "סתומים",
+            "לא תצא",
+            "לא תאכל",
+            "תעמוד בפינה",
+            "תעמדי בפינה",
+            "תעמוד בצד",
+            "תעמדי בצד",
+            "אמא לא תבוא",
+            "אבא לא יבוא",
+            "חסר ערך",
+            "מטומטם",
+            "שקרן",
+            "פחדן",
+            "חסר תועלת",
+            "מגעיל",
+            "עצלן",
+            "עצלנית",
+            "עצלנים",
+            "עצלניות",
+            "חסר ערך",
+            "חסרת ערך",
+            "חסרי ערך",
+            "חסרות ערך",
+            "מטומטם",
+            "מטומטמת",
+            "מטומטמים",
+            "מטומטמות",
+            "שקרן",
+            "שקרנית",
+            "שקרנים",
+            "שקרניות",
+            "פחדן",
+            "פחדנית",
+            "פחדנים",
+            "פחדניות",
+            "מגעיל",
+            "מגעילה",
+            "מגעילים",
+            "מגעילות",
+            "מרושע",
+            "מרושעת",
+            "מרושעים ",
+            "מרושעות",
+            "מפגר ",
+            "מפגרת",
+            "מפגרים",
+            "מפגרות",
+            "שב בשקט",
+            "שבי בשקט",
+            "לא לזוז",
+            "אל תזוז",
+            "אל תזוזו",
+            "אל תזוזי",
+            "מכות"
+            ];
+
+            const shownWords = [];
+
+            badWords.forEach((word, index) => {
+                const results = videoIndex?.videos[0].insights?.transcript.filter(transcript => transcript?.text.includes(word));
+                let instances = [];
+                results.forEach(element => {
+                    instances = [...instances, ...element.instances]
+                });
+                if (instances.length) {
+                    shownWords.push({
+                        "instances": instances,
+                        "type": word,
+                        "id": index
+                    });
+                }
+            });
+
+            if (shownWords?.length) {
+                const fullObj = [
+                    {
+                        "name": "toxicWords",
+                        "displayName": "Toxic Words",
+                        "displayType": "Capsule",
+                        "results":shownWords
+                    }
+                ]
+
+                // Update insights
+                try {
+                    const response = await fetch(`https://api.videoindexer.ai/${location}/Accounts/${accountId}/Videos/${data.id}/Index?language=he-IL`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Authorization': `Bearer ${token}`,
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify([{
+                            value: fullObj,
+                            "path": "/videos/0/insights/customInsights",
+                            "op": "add"
+                          }])
+                    });
+        
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+        
+                    const data = await response.json();
+                    console.log('Video status:', data);
+                } catch (error) {
+                    console.error('Error fetching video status:', error);
+                }
+            }
+    }
 
     const debugInputs = (
         <div style={styles.debugInputs}>
@@ -124,7 +272,7 @@ export function UploadContainer(props) {
             {/*{debugInputs}*/}
             {isLoading && <div className="loader"></div>}
             {!isUploaded && !isLoading && (
-                <input style={styles.filePicker} type="file" accept=".wav" onChange={handleFileUpload}/>
+                <input style={styles.filePicker} type="file" accept=".wav,.mp4,.mp3" onChange={handleFileUpload}/>
             )}
             {videoId && <EmbedContainer videoId={videoId} accountId={accountId} location={location}/>}
         </div>
